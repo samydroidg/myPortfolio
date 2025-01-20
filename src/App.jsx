b678791131e98c9af9b3f14cea86c3dfedcc29f8
samydroidg <gourav.ojha2023@sait.ac.in>
2025-01-20 18:00:27 +0530

    sixth commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Contact from './sections/Contact'
 import Footer from './sections/Footer'
 import WorkExperience from './sections/WorkExperience'
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const App = () => {
   return (
